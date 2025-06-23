daf3527e3d7090e69e8fec4197e0ede1e7b99acb
TLEphage <272229503@qq.com>
2025-06-24 02:10:19 +0800

    重写trie

---
+++ src/cache.h
@@ -0,0 +1,12 @@
+/*
+用域名建一棵 Trie 树，Trie上每个节点维护一条链表保存资源记录，支持尾部插入和随机删除
+使用一条RLU链表将所有资源记录连起来，支持尾部插入和随机删除
+
+*/
+
+#ifndef CACHE_H
+#define CACHE_H
+
+#include "trie.h"
+
+#endif // CACHE_H
--- src/trie.h
+++ src/trie.h
@@ -4,11 +4,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
-// 定义Trie树的节点结构
+#define DOMAIN_MAX_LEN 256
+#define RR_A 1
+#define RR_AAAA 2
+#define RR_CNAME 3
+
+// DNS记录结构
+typedef struct DNSRecord {
+    char domain[DOMAIN_MAX_LEN];
+    time_t expire_time;
+    uint8_t type;
+    union {
+        uint32_t ipv4;              // IPv4 地址
+        uint8_t ipv6[16];           // IPv6 地址
+        char cname[DOMAIN_MAX_LEN]; // CNAME记录
+    } addr;
+
+    struct DNSRecord* trie_next; // 同域名的下一个记录
+    struct DNSRecord* trie_prev; // 同域名的上一个记录
+    struct DNSRecord* lru_next; // LRU链表的下一个节点
+    struct DNSRecord* lru_prev; // LRU链表的上一个节点
+} DNSRecord;
+
+int DNSRecord_compare(const DNSRecord* a, const DNSRecord* b);
+
+// Trie树的节点结构
 typedef struct TrieNode {
     struct TrieNode* children[37];  // 0-9, a-z, .
-    char* ip;                       // IP字段
+    DNSRecord* head;                // 域名对应的DNS记录链表头指针
+    DNSRecord* tail;                // 域名对应的DNS记录链表尾指针
     int isEnd;                      // 是否是域名字符串的末端
     int sum;                        // 包含的有效域名数
 } TrieNode;
@@ -17,13 +43,13 @@ typedef struct TrieNode {
 TrieNode* trie_create();
 
 // 插入域名和IP到Trie树
-void trie_insert(TrieNode* root, const char* domain, const char* ip);
+int trie_insert(TrieNode* root, const char* domain, DNSRecord* record);
+
+// 通过域名查找对应的节点
+TrieNode* trie_search(TrieNode* root, const char* domain);
 
 // 在Trie树中删除域名
-void trie_delete(TrieNode* root, const char* domain);
-
-// 通过域名查找对应的IP
-char* trie_search(TrieNode* root, const char* domain);
+void trie_delete(TrieNode* root, const char* domain, const DNSRecord* record);
 
 // 清理Trie树释放内存
 void trie_free(TrieNode* root);
